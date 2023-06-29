672c9b58304738a5d7803300cdeaa536ff9e133f
Erestor97 <pruano@deloitte.pt>
2023-06-29 15:51:44 +0100

    update

--- Sources/CostumLocationManager/CostumLocationManager.swift
+++ Sources/CostumLocationManager/CostumLocationManager.swift
@@ -1,6 +1,82 @@
-public struct CostumLocationManager {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import CoreLocation
+import Combine
 
-    public init() {
+@available(macOS 10.15, *)
+public class LocationManager {
+    
+    private var locationManager: CLLocationManager
+    private var delegate = LocationManagerDelegate()
+    let currentLocation: PassthroughSubject<CLLocation, Never>
+    
+    init() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = delegate
+        locationManager.allowsBackgroundLocationUpdates = true
+//        locationManager.showsBackgroundLocationIndicator = true
+        currentLocation = delegate.lastLocation
+    }
+    
+    func permissionRequest () {
+        locationManager.requestWhenInUseAuthorization()
+    }
+}
+
+@available(macOS 10.15, *)
+private class LocationManagerDelegate: NSObject, CLLocationManagerDelegate {
+    let lastLocation = PassthroughSubject<CLLocation, Never> ()
+    
+    fileprivate func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        print("Latitude: \(location.coordinate.latitude), Longitude: \(location.coordinate.longitude)")
+        lastLocation.send(location)
+    }
+    
+    fileprivate func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        manager.stopUpdatingLocation()
+        
+        if let clErr = error as? CLError {
+            switch clErr.code {
+            case .locationUnknown, .denied, .network:
+                print("Location request failed with error: \(clErr.localizedDescription)")
+            case .headingFailure:
+                print("Heading request failed with error: \(clErr.localizedDescription)")
+            case .rangingUnavailable, .rangingFailure:
+                print("Ranging request failed with error: \(clErr.localizedDescription)")
+            case .regionMonitoringDenied, .regionMonitoringFailure, .regionMonitoringSetupDelayed, .regionMonitoringResponseDelayed:
+                print("Region monitoring request failed with error: \(clErr.localizedDescription)")
+            default:
+                print("Unknown location manager error: \(clErr.localizedDescription)")
+            }
+        } else {
+            print("Unknown error occurred while handling location manager error: \(error.localizedDescription)")
+        }
+    }
+    
+    @available(macOS 11.0, *)
+    fileprivate func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .restricted:
+            print("Restricted by parental control")
+            
+        case .denied:
+            print("When user select option Dont't Allow")
+            
+        case .authorizedAlways:
+            print("When user select option Change to Always Allow")
+            manager.startUpdatingLocation()
+            
+        case .authorizedWhenInUse:
+            print("When user select option Allow While Using App or Allow Once")
+            manager.requestAlwaysAuthorization()
+            manager.startUpdatingLocation()
+            
+        case .notDetermined:
+            manager.requestAlwaysAuthorization()
+            break
+            
+        default:
+            break
+        }
     }
 }
